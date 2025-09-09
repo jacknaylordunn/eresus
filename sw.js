96740fbf331ec1f940717e59f1e0376456d8d8e6
jacknaylordunn <82784816+jacknaylordunn@users.noreply.github.com>
2025-09-09 23:32:02 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,14 @@
-const CACHE_NAME = 'eresus-cache-v9';
+const CACHE_NAME = 'eresus-cache-v10';
 const urlsToCache = [
   '/',
   'https://cdn.tailwindcss.com',
   'https://esm.sh/react@18.2.0',
   'https://esm.sh/react-dom@18.2.0/client',
-  'https://145955222.fs1.hubspotusercontent-eu1.net/hubfs/145955222/eResus.jpg'
+  'https://14555222.fs1.hubspotusercontent-eu1.net/hubfs/14555222/eResus.jpg',
+  'https://www.resus.org.uk/sites/default/files/2023-08/Post%20cardiac%20arrest%20rehabilitation%20algorithim%202023.png',
+  'https://www.resus.org.uk/sites/default/files/2021-04/als-algorithm-2021-1-724x1024.jpg',
+  'https://www.resus.org.uk/sites/default/files/2021-04/pals-algorithm-2021-724x1024.jpg',
+  'https://www.resus.org.uk/sites/default/files/2021-04/nls-algorithm-2021-724x1024.jpg'
 ];
 
 self.addEventListener('install', event => {
@@ -12,8 +16,15 @@ self.addEventListener('install', event => {
     caches.open(CACHE_NAME)
       .then(cache => {
         console.log('Opened cache');
+        // Use 'no-cors' for third-party resources if they don't support CORS
+        const requests = urlsToCache.map(url => {
+          if (url.startsWith('https://www.resus.org.uk')) {
+            return new Request(url, { mode: 'no-cors' });
+          }
+          return new Request(url, { mode: 'cors' });
+        });
         return Promise.all(
-          urlsToCache.map(url => cache.add(new Request(url, { mode: 'cors' })).catch(err => console.warn('Failed to cache:', url, err)))
+          requests.map(req => cache.add(req).catch(err => console.warn('Failed to cache:', req.url, err)))
         );
       })
   );
